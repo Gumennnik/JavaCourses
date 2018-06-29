d81320dcaac8ed9f623c4044eea7108ac77911f9
evgeniy.buturlia <evgeniy.buturlia@bpmobile.com>
2018-06-29 19:21:17 +0300

    Starting first task

--- homework/src/SecondsSaving.java
+++ homework/src/SecondsSaving.java
@@ -1,12 +1,14 @@
 public class SecondsSaving {
 
 
-    public SecondsSaving(int seconds) {
-        this.seconds = seconds;
+    public SecondsSaving(int input) {
+        this.setSeconds(input);
+        this.setSecondsLeft();
+        return;
     }
 
 
-    private int seconds = 0;
+    private int seconds;
 
     public void setSeconds(int seconds) {
         this.seconds = seconds;
@@ -15,6 +17,57 @@ public class SecondsSaving {
     public int getSeconds() {
         return seconds;
     }
+
+
+    private int secondsLeft;
+    private void setSecondsLeft() {
+        this.secondsLeft = this.seconds%60;
+    }
+    public int getSecondsLeft() {
+        return secondsLeft;
+    }
+
+
+    public int getMins(){
+        int mins = (seconds - secondsLeft)/60;
+        return mins;
+    }
+
+    public int getMinsLeft() {
+        return minsLeft = this.getMins()%60;
+    }
+
+    private int minsLeft ;
+
+    public int getHours(){
+        int hours = (seconds - minsLeft)/60;
+        return hours;
+    }
+
+    public int getHoursLeft() {
+        return hoursLeft;
+    }
+
+    private int hoursLeft = this.getHours()%24;
+    public int getDays()
+    {
+        int days = (this.getHours()-hoursLeft)/24;
+        return days;
+    }
+
+    public int getDaysLeft() {
+        return daysLeft;
+    }
+
+    private int daysLeft = this.getDays()%7;
+    public int getWeeks()
+    {
+        int weeks = (this.getDays()-daysLeft)/7;
+        return weeks;
+    }
+
+
+
 
 
 }
--- homework/src/TaskRunner.java
+++ homework/src/TaskRunner.java
@@ -2,6 +2,16 @@ public class TaskRunner
 {
     public static void main(String[] args) {
 
+        SecondsSaving a = new SecondsSaving(4503);
+        System.out.println(a.getSeconds());
+        System.out.println(a.getSecondsLeft());
+
+
+
+        System.out.println(a.getHours());
+        System.out.println(a.getDays());
+        System.out.println(a.getWeeks());
+        System.out.println(a.getWeeks()+" weeks "+a.getDaysLeft()+" Days "+a.getHoursLeft()+" Hours "+ a.getMinsLeft() + " mins "+ a.getSecondsLeft()+ " seconds ");
     }
 
 }
