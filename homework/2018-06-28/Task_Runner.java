7e22edbe56148477b886be6ef237f528591ce60e
evgeniy.buturlia <evgeniy.buturlia@bpmobile.com>
2018-07-05 14:45:08 +0300

    Debugged

--- homework/2018-06-28/Task_Runner.java
+++ homework/2018-06-28/Task_Runner.java
@@ -6,7 +6,6 @@ public class Task_Runner
         Task2.run(6546787);
         Task3.run(1,2,3);
         Task4.run();
-        Task4.run();
         Task5.run(1234);
         Task6.run(6432);
     }
