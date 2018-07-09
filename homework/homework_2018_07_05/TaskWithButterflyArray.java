2c11f13ef8ddf58b6248c330d56e2e3f7b885a14
Gumennnik <Gumennnik@rambler.ru>
2018-07-10 01:18:19 +0300

    some bugs fixed

--- homework/homework_2018_07_05/TaskWithButterflyArray.java
+++ homework/homework_2018_07_05/TaskWithButterflyArray.java
@@ -48,7 +48,7 @@ public class TaskWithButterflyArray {
         for (int i = 0; i < array.length; i++) {
             System.out.print( i + 1 + ") ");
             for (int j = 0; j < array[0].length ; j++) {
-                System.out.print(array[i][j] + ", ");;
+                System.out.print(array[i][j] + ", ");
             }
             System.out.println(" ");
         }
