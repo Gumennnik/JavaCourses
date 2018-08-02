177dce0653dafabc78eb631038c2f41c2ff2f21b
evgeniy.buturlia <evgeniy.buturlia@bpmobile.com>
2018-08-02 17:58:34 +0300

    added some changes

--- homework/homework_5th_2018_07_19/com/buturlia/hm5/Task_Runner.java
+++ homework/homework_5th_2018_07_19/com/buturlia/hm5/Task_Runner.java
@@ -12,10 +12,18 @@ public class Task_Runner {
 
         List<Student> students = new ArrayList<Student>();
         LinkedList<Student> lLStudents = new LinkedList<Student>();
-        int linkedListTime =  System.currentTimeMillis();
-        for (int i = 0; i < 10 ; i++) {
+        long linkedListTime =  System.currentTimeMillis();
+        for (int i = 0; i < 1000 ; i++) {
             lLStudents.add(new Student( firstNames[random.nextInt(firstNames.length -1 )],secondNames[random.nextInt(secondNames.length -1)], random.nextInt(5), random.nextDouble()*10));
         }
+        linkedListTime = linkedListTime - System.currentTimeMillis();
+
+        ArrayList<Student> arrayListStudents = new ArrayList<>();
+        long arrayListTime =  System.currentTimeMillis();
+        for (int i = 0; i < 1000 ; i++) {
+            lLStudents.add(new Student( firstNames[random.nextInt(firstNames.length -1 )],secondNames[random.nextInt(secondNames.length -1)], random.nextInt(5), random.nextDouble()*10));
+        }
+        arrayListTime = arrayListTime - System.currentTimeMillis();
 
 
 
